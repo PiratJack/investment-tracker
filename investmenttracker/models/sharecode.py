d645e37ca796bb25e4394bde067f08cf14055c36
PiratJack <90952042+PiratJack@users.noreply.github.com>
2022-05-02 11:30:17 +0200

    Added share code validations

---
+++ investmenttracker/models/sharecode.py
@@ -0,0 +1,49 @@
+import gettext
+import sqlalchemy.orm
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float, Date, Enum
+
+from .base import Base, NoPriceException, ValidationException
+from .share import Share
+
+_ = gettext.gettext
+
+
+class ShareCode(Base):
+    __tablename__ = "share_codes"
+    id = Column(Integer, primary_key=True)
+    share_id = Column(Integer, ForeignKey("shares.id"), nullable=False)
+    origin = Column(String(250), nullable=False)
+    value = Column(String(250), nullable=False)
+    share = sqlalchemy.orm.relationship(Share, back_populates="codes")
+
+    @sqlalchemy.orm.validates("share_id")
+    def validate_share_id(self, key, value):
+        self.validate_missing_field(key, value, _("Missing share code share ID"))
+        return value
+
+    @sqlalchemy.orm.validates("origin")
+    def validate_origin(self, key, value):
+        self.validate_missing_field(key, value, _("Missing share code origin"))
+        if len(value) > 250:
+            raise ValidationException(
+                _("Max length for share code origin is 250 characters"),
+                self,
+                key,
+                value,
+            )
+        return value
+
+    @sqlalchemy.orm.validates("value")
+    def validate_value(self, key, value):
+        self.validate_missing_field(key, value, _("Missing share code value"))
+        if len(value) > 250:
+            raise ValidationException(
+                _("Max length for share code value is 250 characters"), self, key, value
+            )
+        return value
+
+    def validate_missing_field(self, key, value, message):
+        if value == "" or value is None:
+            raise ValidationException(message, self, key, value)
+        return value
---
+++ tests/test_sharecode.py
@@ -0,0 +1,96 @@
+import datetime
+import os
+import unittest
+
+import investmenttracker.models.database as databasemodel
+
+from investmenttracker.models.base import NoPriceException, ValidationException
+from investmenttracker.models.share import Share
+from investmenttracker.models.sharecode import ShareCode
+
+DATABASE_FILE = "test.sqlite"
+database = databasemodel.Database(DATABASE_FILE)
+
+try:
+    os.remove(DATABASE_FILE)
+except OSError:
+    pass
+
+
+class TestSharePrice(unittest.TestCase):
+    def setUp(self):
+        self.database = databasemodel.Database(DATABASE_FILE)
+        self.database.session.add_all(
+            [
+                Share(id=1, name="AXA", main_code="FR847238", base_currency="EUR"),
+                Share(id=2, name="Accenture", main_code="ACN", base_currency="USD"),
+                ShareCode(share_id=1, origin="Boursorama", value="1rACN"),
+                ShareCode(share_id=1, origin="Quantalys", value="14587"),
+                ShareCode(share_id=1, origin="AlphaVantage", value="FR4941"),
+            ]
+        )
+        self.database.session.commit()
+
+    def tearDown(self):
+        self.database.session.close()
+        self.database.engine.dispose()
+        os.remove(DATABASE_FILE)
+
+    def test_validations(self):
+        share_code = self.database.share_get_by_id(1).codes[0]
+
+        # Test mandatory fields
+        for field in ["origin", "value"]:
+            for value in ["", None]:
+                test_name = "Share code must have a " + field + " that is not "
+                test_name += "None" if value == None else "empty"
+                with self.assertRaises(ValidationException) as cm:
+                    setattr(share_code, field, value)
+                self.assertEqual(type(cm.exception), ValidationException, test_name)
+                self.assertEqual(
+                    cm.exception.item,
+                    share_code,
+                    test_name + " - exception.item is wrong",
+                )
+                self.assertEqual(
+                    cm.exception.key,
+                    field,
+                    test_name + " - exception.key is wrong",
+                )
+                self.assertEqual(
+                    cm.exception.invalid_value,
+                    value,
+                    test_name + " - exception.invalid_value is wrong",
+                )
+
+        # Test max length of fields
+        for field in ["origin", "value"]:
+            test_name = "Share code " + field + " can't be more than 250 characters"
+            value = "a" * 251
+            with self.assertRaises(ValidationException) as cm:
+                setattr(share_code, field, value)
+            self.assertEqual(type(cm.exception), ValidationException, test_name)
+            self.assertEqual(
+                cm.exception.item,
+                share_code,
+                test_name + " - exception.item is wrong",
+            )
+            self.assertEqual(
+                cm.exception.key,
+                field,
+                test_name + " - exception.key is wrong",
+            )
+            self.assertEqual(
+                cm.exception.invalid_value,
+                value,
+                test_name + " - exception.invalid_value is wrong",
+            )
+
+    def test_relationships(self):
+        share = self.database.share_get_by_id(1)
+        share_code = share.codes[0]
+        self.assertEqual(
+            len(share.codes),
+            3,
+            "ACN share must have 3 codes",
+        )
