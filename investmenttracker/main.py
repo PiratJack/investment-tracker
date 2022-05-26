c3a66476ad353fdc974c098bc0444771d90db981
PiratJack <90952042+PiratJack@users.noreply.github.com>
2022-05-26 16:14:04 +0200

    Removed Wayland-related warnings

--- investmenttracker/main.py
+++ investmenttracker/main.py
@@ -1,6 +1,7 @@
 import gettext
 import PyQt5
 import sys
+import os
 
 import controllers.console
 import controllers.mainwindow
@@ -15,10 +16,13 @@ STYLESHEET_FILE = "./assets/style/app.css"
 gettext.bindtextdomain("messages", LOCALE_FOLDER)
 gettext.translation("messages", localedir=LOCALE_FOLDER).install()
 
+
 # Connect to database
 database = models.database.Database(DATABASE_FILE)
 
 if __name__ == "__main__":
+    # Change platform to avoid Wayland-related warning messages
+    os.environ["QT_QPA_PLATFORM"] = "xcb"
     app = PyQt5.QtWidgets.QApplication(sys.argv)
     with open(STYLESHEET_FILE, "r") as stylesheet:
         app.setStyleSheet(stylesheet.read())
